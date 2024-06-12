6101d2567ec1af99d67a5eb0467b923eb79c097e
gitGutBro <103259112+gitGutBro@users.noreply.github.com>
2024-06-12 16:48:07 +0300

    Changed the ScenesLoader.

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -5,22 +5,22 @@ public class Bootstrap : MonoBehaviour
 {
     [SerializeField] private string _URL;
     [SerializeField] private Image _image;
+    [Space]
+    [SerializeField]private ScenesSwitcher _scenesSwitcher;
 
     private ImageLoader _imageLoader;
     private ResourceLoader _resourceLoader;
-    private ScenesSwitcher _scenesSwitcher;
 
     private void Awake()
     {
         _imageLoader = new();
         _resourceLoader = new();
-        _scenesSwitcher = new();
     }
 
     private async void Start()
     {
         _image.sprite = await _imageLoader.GetLoadedSprite(_URL);
         GameObject gameObject = await _resourceLoader.TryLoad("Resource");
-        _scenesSwitcher.LoadByIndexAsync(1);
+        _scenesSwitcher.SwitchByIndexAsync();
     }
 }
--- Assets/Scripts/ScenesSwitcher.cs
+++ Assets/Scripts/ScenesSwitcher.cs
@@ -1,14 +1,29 @@
+using System;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+[Serializable]
 public class ScenesSwitcher
 {
-    public async void LoadByIndexAsync(int buildIndex)
+    [SerializeField] private Slider _progressBar;
+
+    public async void SwitchByIndexAsync()
     {
-        AsyncOperation loadScene = SceneManager.LoadSceneAsync(buildIndex);
+        AsyncOperation loadScene = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
 
         while (loadScene.isDone == false)
+        {
+            const float AsyncOperationLimit = 0.9f;
+
+            float progress = Mathf.Clamp01(loadScene.progress / AsyncOperationLimit);
+            _progressBar.value = progress;
+
             await UniTask.Yield();
+        }
+
+        System.Threading.Thread.Sleep(500); //For demonstration the progress bar on the scene.
+        loadScene.allowSceneActivation = true;
     }
 }
